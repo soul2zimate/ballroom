aabb772798041aa11a1ab8f6caf4f9a2916d010b
Harald Pehl <harald.pehl@gmail.com>
2014-02-18 11:02:05 +0100

    HAL-301: Add generated ID based on text

--- widgets/src/main/java/org/jboss/ballroom/client/layout/LHSTreeSection.java
+++ widgets/src/main/java/org/jboss/ballroom/client/layout/LHSTreeSection.java
@@ -1,6 +1,5 @@
 package org.jboss.ballroom.client.layout;
 
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TreeItem;
 
 /**
@@ -8,35 +7,28 @@ import com.google.gwt.user.client.ui.TreeItem;
  */
 public class LHSTreeSection extends TreeItem {
 
+    private final static String ID_PREFIX = "nav_section_";
+
     public LHSTreeSection(String title) {
         this(title, false);
     }
 
     public LHSTreeSection(String title, boolean first) {
-
+        super();
         addStyleName("tree-section");
-
         setText(title);
+        getElement().setId(ID_PREFIX + title.replace(' ', '_').toLowerCase());
 
-        if(first)
-        {
+        if (first) {
             addStyleName("tree-section-first");
             getElement().setAttribute("tabindex", "-1");
         }
     }
 
 
-
     @Override
     public void setSelected(boolean selected) {
-
         super.setSelected(selected);
-        if(selected)
-            addStyleName("tree-section-selected");
-        else
-            removeStyleName("tree-section-selected");
+        if (selected) { addStyleName("tree-section-selected"); } else { removeStyleName("tree-section-selected"); }
     }
-
-
 }
-
